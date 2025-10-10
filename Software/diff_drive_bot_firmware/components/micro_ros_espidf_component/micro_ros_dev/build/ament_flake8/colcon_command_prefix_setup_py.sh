37e75d255037137f0a2db63a5ad70aeab842f94c
Zander Mao <zma40@sfu.ca>
2025-10-09 21:16:31 -0700

    remove build files

--- Software/diff_drive_bot_firmware/components/micro_ros_espidf_component/micro_ros_dev/build/ament_flake8/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/Users/zdrrrm/Desktop/Projects/AIROBOT/Software/firmware/components/micro_ros_espidf_component/micro_ros_dev/install/ament_lint/share/ament_lint/package.sh"
--- Software/diff_drive_bot_firmware/components/micro_ros_espidf_component/micro_ros_dev/build/ament_lint/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/Users/zdrrrm/.espressif/python_env/idf5.5_py3.10_env':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/Users/zdrrrm/Desktop/Projects/AIROBOT/Software/firmware/components/micro_ros_espidf_component/micro_ros_dev/install/ament_lint'
