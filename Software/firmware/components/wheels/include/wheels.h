f8b91d947e8dda513e553933068e084d5d628f13
Zander-M <zm800@nyu.edu>
2026-01-18 18:09:35 -0800

    adding encoder count pub messages

--- Software/firmware/components/wheels/include/wheels.h
+++ Software/firmware/components/wheels/include/wheels.h
@@ -1,7 +1,6 @@
 // Wheel Functions
 #include <esp_attr.h>
 
-
 typedef struct {
     float kp, ki, kd;
 } PIDConfig;
@@ -9,5 +8,6 @@ typedef struct {
 void wheel_init();
 void wheel_setPID(PIDConfig cfg);
 void wheel_run();
+void wheel_get_counts(int64_t* left, int64_t* right);
 void setLeftTarget(float target);
 void setRightTarget(float target);
