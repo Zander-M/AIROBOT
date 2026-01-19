6d4a8447b23e1eddccb9f5988b02245e3de1b4f2
Zander-M <zm800@nyu.edu>
2026-01-18 18:43:54 -0800

    fix wheel radius

--- Software/firmware/components/robot_params/include/robot_params.h
+++ Software/firmware/components/robot_params/include/robot_params.h
@@ -4,10 +4,10 @@
 /********* HARDWARE DEFINITION *********/
 
 // Robot geometry
-#define WHEEL_RADIUS      0.042f   // meters
+#define WHEEL_RADIUS      0.022f   // meters
 #define WHEEL_BASE        0.10f   // distance between wheels (m)
-#define TICKS_PER_REV     2660.0f  // encoder CPR (counts per wheel revolution 1:40)
-// #define TICKS_PER_REV     350.0f  // encoder CPR (counts per wheel revolution 1:300)
+// #define TICKS_PER_REV     2660.0f  // encoder CPR (counts per wheel revolution 1:40)
+#define TICKS_PER_REV     350.0f  // encoder CPR (counts per wheel revolution 1:300)
 #define TWO_PI 6.28318530718f
 #define PWM_MIN           120
 #define PWM_MAX           255
